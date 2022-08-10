9c34a64062c99d8c4c20d8e3719bc646e5e741ec
Kenneth Okalang <okalang.ok@gmail.com>
2022-08-11 00:22:42 +0200

    Add project files.

---
+++ LeaveAndEmploymentWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveAndEmploymentWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
