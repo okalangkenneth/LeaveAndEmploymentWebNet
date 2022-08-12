c4209b02b1e3582f6a19751be73c179e0a2946a9
Kenneth Okalang <okalang.ok@gmail.com>
2022-08-12 11:48:50 +0200

    Modified the CRUD screens and added datatables and swal. End of Leave type module.

---
+++ LeaveAndEmploymentWeb/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveAndEmploymentWeb.Data;
+using LeaveAndEmploymentWeb.Models;
+
+namespace LeaveAndEmploymentWeb.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
